aeed208950128468c94ec0ae7a433e3cbc6ec32b
amir2008bai <1mechta0912@gmail.com>
2026-06-19 08:40:13 +0300

    Revert "fix memory build"
    This reverts commit ae7be25fc5e3a2cb98d8a38197bfefe32c80ec57.

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'api.dicebear.com'],
   },
-  experimental: {
-    workerThreads: false,
-    cpus: 1,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
